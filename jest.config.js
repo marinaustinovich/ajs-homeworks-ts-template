64268b36808f4f9075cf2f7126aaa35472e97243
Marina Ustinovich <marishka0509@mail.ru>
2023-03-02 10:51:35 +0300

    fixed jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -3,8 +3,8 @@ module.exports = {
   verbose: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
-  moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx']
-  modulePaths: ['<rootDir>/src'],
+  moduleFileExtensions: ['js', 'jsx', 'json', 'ts', 'tsx'],
+  // modulePaths: ['<rootDir>/src'],
   coverageDirectory: 'coverage',
-  coveragePathIgnorePatterns: ['<rootDir>/dist/', '/node_modules/'],
+  coveragePathIgnorePatterns: ['<rootDir>/dist/', '/node_modules/',  '!*.d.ts'],
 };
